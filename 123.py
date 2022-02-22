1b01ceb27e245fffa73078f9f8cdc6503e807c00
ZZGNAW <74235000+ZZGNAW@users.noreply.github.com>
2022-02-22 11:26:02 +0800

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,18 @@
+>>> my_name = str(input())
+Irving
+>>> my_age = int(input())
+22
+>>> my_street = str(input())
+Wenzhou
+>>> my_sport = str(input())
+Baseball
+print("Hello, my name is :" + my_name +'\n' +
+      "I am " + str(my_age) +" years\n" +
+      "I live in " + my_street + '\n'
+      "I like to play " + my_sport)
+
+Result:
+Hello, my name is :Irving
+I am 22 years
+I live in Wenzhou
+I like to play Baseball
